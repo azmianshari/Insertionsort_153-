c283de8dd6b8a114888fc2789490fd9856dda2cd
azmianshari <azmianshari123@gmail.com>
2023-03-24 10:30:47 +0700

    Membuat Array

--- Insertion sort/Insertion sort.cpp
+++ Insertion sort/Insertion sort.cpp
@@ -7,19 +7,19 @@ int n;					// Membuat Variable inputan n
 void input() {
 	while (true)
 	{
-		cout << "Masukan Jumlah Data pada Array : ";
-		cin >> n;
+		cout << "Masukan Jumlah Data pada Array : ";	 // Membuat inputan jumlah element Array
+		cin >> n;										// Memanggil variabel inputan n
 
-		if (n <= 20) {
+		if (n <= 20) {									// Membuat Kondisi n tidak Lebih dari 20
 			break;
 		}
 		else
 		{
-			cout << "\nArray yang anda masukan maksimal 20 elemen.\n";
+			cout << "\nArray yang anda masukan maksimal 20 elemen.\n"; // Menampilkan Pesan jika data lebih dari 20
 		}
 	}
 	cout << endl;
-	cout << "=====================" << endl;
-	cout << "Masukan Element Array" << endl;
+	cout << "=====================" << endl;			// Membuat jarak per baris program
+	cout << "Masukan Element Array" << endl;			// Membuat susunan data element Array
 	cout << "=====================" << endl;
 }
