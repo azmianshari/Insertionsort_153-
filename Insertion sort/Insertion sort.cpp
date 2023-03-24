551230537fc3bf1e3e081751f31dbb45e16bd207
azmianshari <azmianshari123@gmail.com>
2023-03-24 10:58:44 +0700

    Membuat jarak perbaris dan Membuat susunan data element Array

--- Insertion sort/Insertion sort.cpp
+++ Insertion sort/Insertion sort.cpp
@@ -22,4 +22,38 @@ void input() {			// Procedure Input
 	cout << "=====================" << endl;			// Membuat jarak per baris program
 	cout << "Masukan Element Array" << endl;			// Membuat susunan data element Array
 	cout << "=====================" << endl;
+
+	for (int i = 0; i < n; i++)							// Menggunakan perulangan for untuk menyimpan data pada Array
+	{
+		cout << "Data ke-" << (i + 1) << ": ";			// Memasukan atau menginput nilai data n
+		cin >> arr[i];									// Menyimpan nilai data n kedalam Array arr
+	}
+}
+
+void insertionsort() {									// Procedure Insertionsort
+
+	int temp;											// Membuat Variabel data temporer atayu menyimpan sementara
+	int j;												// Membuat Variabel j sebagai penanda
+
+	for (int i = 1; i < n; i++) {							// 1. Looping dengan i dimulai dari 1 hingga n-1
+
+		temp = arr[i];									// 2. Simpan nilai arr[1] ke Variabel sementara
+
+		j = 1 - 1;										// 3. Setting nilai j sama dengan i-1;
+
+		while (j >= 0 && arr[j] > temp)					// 4. Looping while dimana j lebih besar sama dengan 0 dan arr[j] lebih besar daripada temp
+		{
+			arr[j + 1] = arr[j];						// 4a. Simmpan arr[j] kedalam Variabel arr[j+1]
+			j--;										// 4b. Decrement nilai j by 1
+		}
+
+		arr[j + 1] = temp;								// Simpan nilai temp ke dalam arr[j+1]
+
+
+		cout << "\nPass " << i << ": ";					// Output ke layar
+		for (int k = 0; k < n; k++) {					// Looping nilai k dimulai dari 0 hingga n-1
+			cout << arr[k] << " ";						// Output ke layar
+		}
+	}	
+	
 }
